e1edf937c2ce4088d6cb6f1b1693a1a938d44c5a
Shreyas Desai <81674158+Shreyas-Sara@users.noreply.github.com>
2021-09-01 16:39:57 +0530

    Add files via upload

--- login_page.js
+++ login_page.js
@@ -1,5 +1,5 @@
 function lin_btn(){
     n = document.getElementById("user_name").value;
     localStorage.setItem(user_name, n);
-    window.location = "page.html";
+    window.location = "main_page.html";
 }
---
+++ main_page.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyADsinu-X2EeF6rmJgtsQ36DI-an-xJNWI",
+    authDomain: "let-s-chat-web-app-e5c18.firebaseapp.com",
+    projectId: "let-s-chat-web-app-e5c18",
+    storageBucket: "let-s-chat-web-app-e5c18.appspot.com",
+    messagingSenderId: "55946298595",
+    appId: "1:55946298595:web:7655edac2b9fbd32f431d6"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
